50932c41d566e911f6a3cfcf7753444578ec3a5a
heroeslure <heroesluregame@gmail.com>
2024-10-03 20:49:18 +0100

    added middleware.js

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { withAuth } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
+// import { useSession } from "next-auth/react";
+// import { NextRequest } from "next/server";
+
+export default withAuth({
+  pages: {
+    signIn: "/",
+  },
+  callbacks: {
+    authorized: ({ req }) => {
+      // verify token and return a boolean
+      const sessionToken = req.cookies.get("next-auth.session-token");
+      if (sessionToken) return true;
+      else return false;
+    },
+  },
+});
+
+export const config = {
+  matcher: ["/users/:path*", "/conversations/:path*"],
+};
