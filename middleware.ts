3fd096ca38ff8af2d2a59823f9f1f2da638207f4
heroeslure <heroesluregame@gmail.com>
2024-10-04 09:18:15 +0100

    P:middleware.ts final fix

--- middleware.ts
+++ middleware.ts
@@ -10,7 +10,10 @@ export function middleware(request: NextRequest) {
   const protectedRoutes = ["/users", "/conversations"];
 
   if (protectedRoutes.some((route) => pathname.startsWith(route))) {
-    const token = request.cookies.get("next-auth.session-token");
+    if (!process.env.NEXTAUTH_COOKIE_AUTH_NAME) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+    const token = request.cookies.get(process.env.NEXTAUTH_COOKIE_AUTH_NAME);
 
     if (!token) {
       return NextResponse.redirect(new URL("/", request.url));
